dbb0beb4137b8456c1eeb759e8ff6e354128fe17
larchick <liliyagreis@gmail.com>
2023-07-09 16:28:57 +0300

    1

---
+++ com/example/kursovayarabota2/Kursovayarabota2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovayarabota2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovayarabota2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovayarabota2Application.class, args);
+    }
+
+}
